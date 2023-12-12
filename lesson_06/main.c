47382e102633ac96fdf8be74e634331b1cbc6c25
JohnReisL <roma_alekseev@ngs.ru>
2023-12-13 01:07:44 +0700

    Добавлен прототип для будуюшей структуры и функций для работы с ней.

---
+++ lesson_06/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+struct MyTable {
+  char surename[64];
+  unsigned long long record_book_number;
+  char faculty_name[16];
+  char study_group_name[16]
+};
+
+void InputToTable(struct MyTable table) {
+  
+}
+
+void FromFileToTable(struct MyTable table) {
+  
+}
+
+void FromTableToFile(char file_name[]) {
+  
+}
+
+void PrintTableTable(struct MyTable table) {
+
+}
+
+void SortByColumn(struct MyTable table, unsigned colum_number) {
+
+}
+
+void FindByValue(struct MyTable table,
+		 unsigned colum_number,
+		 char value[]) {
+  
+}
+
+void DeleteLine(struct MyTable table,
+		unsigned line_number) {
+  
+}
+
+void ChangeLine(struct MyTable table,
+		unsigned line_number) {
+  
+}
+
+
+int main() {
+  
+  return 0;
+}
